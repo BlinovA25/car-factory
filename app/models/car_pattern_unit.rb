3bd8200fa658b56ec1d394476f898f0d25a075e4
anton <BlinovA25>
2022-12-12 01:39:42 +0300

    Added unique? method to car_pattern_unit.rb

--- app/models/car_pattern_unit.rb
+++ app/models/car_pattern_unit.rb
@@ -1,4 +1,9 @@
 class CarPatternUnit < ApplicationRecord
   belongs_to :car_pattern
   belongs_to :part
+
+  # Checks if CarPatternUnit is unique
+  def unique?
+    CarPatternUnit.find_by(car_pattern_id: car_pattern_id, part_id: part_id).nil?
+  end
 end
