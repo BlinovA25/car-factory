9cb4faa66280f95059aa9fa2a1ada6701e0c81ed
anton <BlinovA25>
2022-12-19 18:53:27 +0300

    Update method still need to be fixed

--- app/controllers/car_pattern_units_controller.rb
+++ app/controllers/car_pattern_units_controller.rb
@@ -1,6 +1,7 @@
 class CarPatternUnitsController < ApplicationController
   before_action :set_car_pattern_unit, only: [:show, :update, :destroy]
-  before_action :set_car_pattern, only: [:create]
+  before_action :set_car_pattern, only: [:create, :update, :destroy]
+  before_action :set_part, only: [:create]
 
   # GET /car_pattern_units
   def index
@@ -16,17 +17,43 @@ class CarPatternUnitsController < ApplicationController
 
   # POST /car_pattern_units
   def create
-    @car_pattern_unit = CarPatternUnit.new(car_pattern_id: params[:car_pattern_id], part_id: car_pattern_unit_params[:part_id])
-    if @car_pattern_unit.save && unique?
+    @car_pattern_unit = CarPatternUnit.new(car_pattern_id: CarPattern.next_id,
+                                           part_id: car_pattern_unit_params[:part_id],
+                                           quantity: car_pattern_unit_params[:quantity])
+
+    p @car_pattern_unit
+
+    return unless @car_pattern.actual? && @part.actual? && @car_pattern_unit.unique?
+
+    if @car_pattern.copy && @car_pattern_unit.save
       render json: @car_pattern_unit, status: :created
     else
       render json: @car_pattern_unit.errors, status: :unprocessable_entity
     end
   end
 
+  # TODO: add check for part uniqueness and car_pattern actuality
   # PATCH/PUT /car_pattern_units/1
   def update
-    if @car_pattern_unit.update(car_pattern_unit_params)
+    @new_car_pattern_unit = CarPatternUnit.new(@car_pattern_unit.dup.attributes)
+    @new_car_pattern_unit.assign_attributes(car_pattern_unit_params)
+    @new_car_pattern_unit.assign_attributes(car_pattern_id: CarPattern.next_id)
+
+    return if @new_car_pattern_unit.nil?
+
+    #    render json: "SUCCESS
+    #
+    # OLD ID #{@car_pattern_unit.id}
+    # OLD Part #{@car_pattern_unit.part_id}
+    # OLD CarPattern #{@car_pattern_unit.car_pattern_id}
+    # OLD Quantity #{@car_pattern_unit.quantity}
+    #
+    # NEW ID #{@new_car_pattern_unit.id}(null before save)
+    # NEW Part #{@new_car_pattern_unit.part_id}
+    # NEW CarPattern #{@new_car_pattern_unit.car_pattern_id}
+    # NEW Quantity #{@new_car_pattern_unit.quantity}"
+
+    if @car_pattern.copy && @new_car_pattern_unit.save && Part.find(@new_car_pattern_unit.part_id).actual?
       render json: @car_pattern_unit
     else
       render json: @car_pattern_unit.errors, status: :unprocessable_entity
@@ -35,14 +62,16 @@ class CarPatternUnitsController < ApplicationController
 
   # DELETE /car_pattern_units/1
   def destroy
-    @car_pattern_unit.destroy
+    return unless @car_pattern_unit.actual?
+
+    new_car_pattern = @car_pattern.copy
+    CarPatternUnit.find_by(car_pattern_id: new_car_pattern.id, part_id: @car_pattern_unit.part_id).destroy
   end
 
   private
 
-  # Checks if CarPatternUnit is unique
-  def unique?
-    CarPatternUnit.where(car_pattern_id: @car_pattern.id, part_id: @car_pattern_unit.part_id).count < 1
+  def set_part
+    @part = Part.find(car_pattern_unit_params[:part_id]) unless car_pattern_unit_params[:part_id].nil?
   end
 
   # Use callbacks to share common setup or constraints between actions.
@@ -56,6 +85,6 @@ class CarPatternUnitsController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def car_pattern_unit_params
-    params.require(:car_pattern_unit).permit(:part_id)
+    params.require(:car_pattern_unit).permit(:part_id, :quantity)
   end
 end
--- app/models/car_pattern_unit.rb
+++ app/models/car_pattern_unit.rb
@@ -2,6 +2,8 @@ class CarPatternUnit < ApplicationRecord
   belongs_to :car_pattern
   belongs_to :part
 
+  include AncestorsConcern
+
   # Checks if CarPatternUnit is unique
   def unique?
     CarPatternUnit.find_by(car_pattern_id: car_pattern_id, part_id: part_id).nil?
