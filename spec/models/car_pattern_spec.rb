b3f79a4fc5fe388fda26754e1780b7bf55bf3d7a
anton <BlinovA25>
2023-01-17 20:49:08 +0300

    new tests

--- spec/models/car_pattern_spec.rb
+++ spec/models/car_pattern_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe CarPattern, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-
-  it "test tests" do
+  it 'first test' do
     expect(false).to be(false)
   end
 
+  it 'Expects that child is nil for new CarPattern' do
+    car_pattern = FactoryBot.create(:car_pattern)
+    expect(car_pattern.child).to be nil
+  end
+
+  it 'car_pattern concern logic works clear' do
+    car_pattern = FactoryBot.create(:car_pattern)
+
+    expect(car_pattern.name).to be car_pattern_copy.name
+  end
 end
