bc3507dfc459725078ceaffb66aa49405b124cb6
anton <BlinovA25>
2023-01-17 17:51:40 +0300

    added rspec for car_pattern

---
+++ spec/models/car_pattern_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe CarPattern, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "test tests" do
+    expect(false).to be(false)
+  end
+
+end
