e2819317355ba82cb583b1a3d836f7b706e3d1a6
anton <BlinovA25>
2023-01-18 10:26:04 +0300

    db cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'database_cleaner/active_record'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -26,6 +27,9 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
+
+  DatabaseCleaner.strategy = :truncation
+  DatabaseCleaner.clean
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
