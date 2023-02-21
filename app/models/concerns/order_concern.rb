44eab32b7dd7ee12535166de61e7a8ecbdddd9e4
anton <BlinovA25>
2023-02-21 21:54:23 +0300

    added set_order method

--- app/models/concerns/order_concern.rb
+++ app/models/concerns/order_concern.rb
@@ -2,8 +2,17 @@ module OrderConcern
   extend ActiveSupport::Concern
 
   included do
+    def set_order(union_by = nil)
+      unless union_by.nil?
+        @objects = self.class.where(union_by: self.car)
+      else
+        @objects = self.class.all
+      end
 
+    end
   end
+
+  # TODO:  car_pattern_unit.set_order(car_pattern_id)
 
   # Class methods
   class_methods do
